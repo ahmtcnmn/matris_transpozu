e27adeb1f414be588bd509c6f89151cbf623cdd0
Ahmet Can Menemencioğlu <95551059+ahmtcnmn@users.noreply.github.com>
2023-03-15 23:28:12 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        int a,b,c,d;
+        //System.out.printf("Oluşturmak istediğiniz matris transpozunun değerlerini giriniz");
+        //System.out.print("Sütun sayısını giriniz : ");
+        //a=input.nextInt();
+        //System.out.print("Kolon sayısını giriniz : ");
+        //b=input.nextInt();
+        //c=b;
+        //d=a;
+        int[][] matris=new int[3][2];
+        int[][] transpoz=new int[2][3];
+
+
+
+
+
+    }
+}
